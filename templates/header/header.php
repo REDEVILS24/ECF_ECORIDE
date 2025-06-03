3189732c0cc0a43a19aec786476dc8ceb8d9c4cd
REDEVILS24 <taieb.mimouni@hotmail.fr>
2025-06-03 15:24:55 +0200

    Mise en place de l'architecture et création du header

---
+++ templates/header/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/templates/style.css">
+    <title>ECORIDE</title>
+</head>
+
+<body>
+    <section class="head">
+        <nav class="navigation">
+            <img src="/IMAGE/ICONE/logoEcoRide.jpg" width="100" alt="Ecoride logo" class="logo">
+            <div class="rechercher">
+                <input type="search" name="recherche" class="recherche">
+                <button class="btn_recherche">Rechercher</button>
+            </div>
+            <h1>ECORIDE</h1>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Covoiturage</a></li>
+                <li><a href="#">Connexion</a></li>
+                <li><a href="#">Deconnexion</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </section>
+</body>
+
+</html>
